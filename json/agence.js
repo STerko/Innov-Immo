21243faf1c3540d8fa54dec632ad6561ed8625b4
STerko <151834086+STerko@users.noreply.github.com>
2024-02-06 18:41:04 +0100

    Add files via upload

---
+++ json/agence.js
@@ -0,0 +1,44 @@
+  var lesdata = `[
+    {
+    "id":1,
+    "nom":"Paris'loc",
+    "lieu":"Paris",
+    "respNom":"DUMONT Fabrice",
+    "respCivil":"M",
+    "personnel":["Charlie","Stéphanie","Romain"],
+    "photo":"prof4.jpg",
+    "tel":"03 82 45 71 96",
+    "mel":"dumont.fabrice@gmail.com"},
+
+    {
+      "id":2,
+      "nom":"Rennes'Loc",
+      "lieu":"Rennes",
+      "respNom":"DUBOIS Eric",
+      "respCivil":"M",
+      "personnel":["Aurélie","Enzo","Pierre","Xayah"],
+      "photo":"prof2.jpg",
+      "tel":"03 85 45 78 94",
+      "mel":"dubois.eric@gmail.com"},
+      
+      {
+        "id":3,
+        "nom":"Marseille'Loc",
+        "lieu":"Marseille",
+        "respNom":"CHAZAL Claire",
+        "respCivil":"Mme",
+        "personnel":["Eva","Enola","Mathéo"],
+        "photo":"prof1.jpg",
+        "tel":"03 85 45 75 91",
+        "mel":"chazal.claire@gmail.com"},
+        
+        {
+          "id":4,
+          "nom":"Toulouse'Loc",
+          "lieu":"Toulouse",
+          "respNom":"HAMBERT Sophie",
+          "respCivil":"Mme",
+          "personnel":["Julien","Xerath","Lolita"],
+          "photo":"prof3.jpg",
+          "tel":"03 85 45 76 93",
+          "mel":"hambert.sophie@gmail.com"}]`; 
